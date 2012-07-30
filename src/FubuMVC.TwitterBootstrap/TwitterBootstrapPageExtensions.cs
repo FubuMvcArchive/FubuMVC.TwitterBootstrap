1092942d7b9ad4744bd3d6645bd6efe03be1be3a
Jeremy Miller <jeremydmiller@yahoo.com>
2012-07-30 09:23:57 -0500

    activation js file for the collapsible partial

--- src/FubuMVC.TwitterBootstrap/TwitterBootstrapPageExtensions.cs
+++ src/FubuMVC.TwitterBootstrap/TwitterBootstrapPageExtensions.cs
@@ -26,19 +26,19 @@ namespace FubuMVC.TwitterBootstrap
 
         public static CollapsiblePartialExpression CollapsiblePartialFor<TInputModel>(this IFubuPage page) where TInputModel : class
         {
-            page.Asset("twitter/bootstrap-collapse.js");
+            page.Asset("twitter/activate-collapsible.js");
             return new CollapsiblePartialExpression(() => page.Partial<TInputModel>());
         }
 
         public static CollapsiblePartialExpression CollapsiblePartialFor(this IFubuPage page, object input)
         {
-            page.Asset("twitter/bootstrap-collapse.js");
+            page.Asset("twitter/activate-collapsible.js");
             return new CollapsiblePartialExpression(() => page.PartialFor(input));
         }
 
         public static CollapsiblePartialExpression CollapsiblePartialFor<TInputModel>(this IFubuPage page, TInputModel model) where TInputModel : class
         {
-            page.Asset("twitter/bootstrap-collapse.js");
+            page.Asset("twitter/activate-collapsible.js");
             return new CollapsiblePartialExpression(() => page.PartialFor(model));
         }
     }
